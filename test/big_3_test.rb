f531a414f7c595eb1ae745324011e35112d1b1fc
Anthony Seliga <anthony.seliga@gmail.com>
2017-10-18 00:15:10 -0400

    add team and player stats tests

--- test/big_3_test.rb
+++ test/big_3_test.rb
@@ -1,11 +1,22 @@
 require "test_helper"
 
 class Big3Test < Minitest::Test
+  def setup
+    @stats = Big3::League.new
+  end
+
+  def test_team_stats
+    assert true, @stats.team_stats
+  end
+
+  def test_player_name_nil?
+    assert_raise NameError do
+      puts @stats.player_stats
+    end
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Big3::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
