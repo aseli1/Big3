05a08db5192536c61bf15b83c7539319964510c2
Anthony <anthony.seliga@gmail.com>
2017-10-19 23:00:08 -0400

    add player stats and roster

--- test/big_3_test.rb
+++ test/big_3_test.rb
@@ -6,17 +6,26 @@ class Big3Test < Minitest::Test
   end
 
   def test_team_stats
-    assert true, @stats.team_stats
+    assert_instance_of Hash, @stats.team_stats
   end
 
   def test_player_name_nil?
-    assert_raise NameError do
-      puts @stats.player_stats
+    exception = assert_raises NameError do
+      @stats.player_stats
     end
+    assert_equal("player_name is not defined", exception.message)
+  end
+
+  def test_player_stats
+    @stats.player_name = "Mike Bibby"
+    assert_instance_of Hash, @stats.player_stats
+  end
+
+  def test_rosters
+    assert_instance_of Hash, @stats.rosters
   end
 
   def test_that_it_has_a_version_number
     refute_nil ::Big3::VERSION
   end
-
 end
